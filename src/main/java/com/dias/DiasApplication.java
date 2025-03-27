904448400fc68e76b02abb55de7e344f6dbc7d7a
biskuvii <ertugrul.elibol55@gmail.com>
2025-03-27 14:08:53 +0300

    chore!: DiasApplication annotations

--- src/main/java/com/dias/DiasApplication.java
+++ src/main/java/com/dias/DiasApplication.java
@@ -2,8 +2,15 @@ package com.dias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.dias")
+@EntityScan(basePackages = "com.dias")
+@EnableJpaRepositories(basePackages = "com.dias")
 public class DiasApplication {
 
     public static void main(String[] args) {
